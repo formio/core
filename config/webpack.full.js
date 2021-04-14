fc1a5109394432437829674bec490d3a71209b4e
Travis Tidwell <travis@form.io>
2021-04-13 19:49:29 -0500

    Updated build and version.

--- config/webpack.full.js
+++ config/webpack.full.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const config = require('./webpack.config');
 config.mode = 'production';
-config.entry = {'formio.core.min.js': './src/core.ts'};
+config.entry = {'formio.full.min.js': './src/core.ts'};
 config.output.path = path.resolve(__dirname, '../dist');
 config.output.library.export = 'Formio';
 module.exports = config;
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,6 +2,7 @@ const path = require('path');
 const config = require('./webpack.config');
 config.mode = 'production';
 config.entry = {
+  'formio.core.min.js': './src/index.ts',
   'formio.base.min.js': './src/base/index.ts',
   'formio.components.min.js': './src/components/index.ts',
   'formio.model.min.js': './src/model/index.ts',
