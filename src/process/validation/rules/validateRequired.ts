f279fcd2ac8601896b36e6b4929977b4f7ea3b22
brendanjbond <brendanbond@gmail.com>
2023-07-09 12:28:26 -0500

    change order of the required validation to check for empty objects first

--- src/process/validation/rules/validateRequired.ts
+++ src/process/validation/rules/validateRequired.ts
@@ -12,7 +12,13 @@ export const validateRequired: RuleFn = async (context) => {
     const error = new FieldError('required', context);
     const { component, value } = context;
     if (component.validate?.required) {
-        if (Array.isArray(value) && value.length === 0) {
+        if (
+            (value === null || value === undefined || isEmptyObject(value) || !!value === false) &&
+            !component.hidden
+        ) {
+            return error;
+        }
+        else if (Array.isArray(value) && value.length === 0) {
             return error;
         }
         else if (component.type === 'address' && isAddressComponentDataObject(value)) {
@@ -23,12 +29,6 @@ export const validateRequired: RuleFn = async (context) => {
         }
         else if (typeof value === 'object' && value !== null) {
             return Object.values(value).some((val) => !!val) ? null : error;
-        }
-        if (
-            (value === null || value === undefined || isEmptyObject(value) || !!value === false) &&
-            !component.hidden
-        ) {
-            return error;
         }
     }
     return null;
