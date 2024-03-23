20307d45f9b4bc6ba83e63ec2c352e4c9d19ed34
Travis Tidwell <travis@form.io>
2024-03-23 08:20:28 -0500

    Changes to experimental exports.

--- src/experimental/index.ts
+++ src/experimental/index.ts
@@ -1,4 +1,7 @@
-export * from './template';
-export * from './core';
+import FormioCore from './core';
 export * from './base';
 export * from './model';
+export * from './components';
+export * from './template';
+export { FormioCore as Formio };
+export default FormioCore;
