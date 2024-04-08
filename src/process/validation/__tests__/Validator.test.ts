d4e44dd0fd46e8e9fd2efab23e22fced987f1a38
brendanjbond <brendanbond@gmail.com>
2024-04-08 09:35:25 -0500

    fix url validation and add tests

--- src/process/validation/__tests__/Validator.test.ts
+++ src/process/validation/__tests__/Validator.test.ts
@@ -1,8 +1,9 @@
 import { expect } from 'chai';
-import { validateProcess } from '../index';
+import { interpolateErrors, validateProcess } from '../index';
 import { rules } from "../rules";
 import { simpleForm } from './fixtures/forms';
 import { ProcessorType, ValidationScope } from 'types';
+import { FieldError, InterpolateErrorFn } from 'error';
 import get from 'lodash/get';
 
 it('Validator will throw the correct errors given a flat components array', async () => {
@@ -38,10 +39,16 @@ it('Validator will throw the correct errors given a flat components array', asyn
 });
 
 it('Validation errors (FieldErrors) should include the rule name mapping in the "validator" param', async () => {
-    let errors: string[] = [];
     const data = {
-        requiredField: ''
+        requiredField: '',
+        maximumWords: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        minimumWords: 'Hello',
+        email: 'brendanb',
+        url: 'htpigoogle',
+        inputMask: 'hello, world',
+        submit: false,
     };
+    const result: Map<string, ReturnType<typeof interpolateErrors>> = new Map();
     for (let component of simpleForm.components) {
         const path = component.key;
         const scope: ValidationScope = { errors: [] };
@@ -55,9 +62,18 @@ it('Validation errors (FieldErrors) should include the rule name mapping in the 
             processor: ProcessorType.Validate,
             rules
         });
-        if (scope.errors) {
-            errors = [...errors, ...scope.errors.map((error) => error.errorKeyOrMessage)];
-        }
+        result.set(path, interpolateErrors(scope.errors));
     }
-    console.log(errors);
+    expect(result.get('requiredField')).to.have.length(1);
+    expect(result.get('requiredField')![0].context.validator).to.equal('required');
+    expect(result.get('maximumWords')).to.have.length(1);
+    expect(result.get('maximumWords')![0].context.validator).to.equal('maxWords');
+    expect(result.get('minimumWords')).to.have.length(1);
+    expect(result.get('minimumWords')![0].context.validator).to.equal('minWords');
+    expect(result.get('email')).to.have.length(1);
+    expect(result.get('email')![0].context.validator).to.equal('email');
+    expect(result.get('url')).to.have.length(1);
+    expect(result.get('url')![0].context.validator).to.equal('url');
+    expect(result.get('inputMask')).to.have.length(1);
+    expect(result.get('inputMask')![0].context.validator).to.equal('inputMask');
 });
