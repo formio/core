da89bf97e04be50b721eae93ad80aae7f0e24a3f
Hanna Kurban <hannak@form.io>
2024-05-27 19:51:49 +0300

    FIO-8027 added Cloudflare Turnstile as a captcha Provider

--- src/process/validation/rules/databaseRules.ts
+++ src/process/validation/rules/databaseRules.ts
@@ -1,9 +1,7 @@
 import { ValidationRuleInfo } from "types";
 import { validateUniqueInfo } from "./validateUnique";
-import { validateCaptchaInfo } from "./validateCaptcha";
 
 // These are the validations that require a database connection.
 export const databaseRules: ValidationRuleInfo[] = [
-    validateUniqueInfo,
-    validateCaptchaInfo
+    validateUniqueInfo
 ];
