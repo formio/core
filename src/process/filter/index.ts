723083cc0df5641ea388a0aceb4781c7223160cb
John Teague <johnt@formio.com>
2024-06-24 13:34:55 -0500

    verified dataGrid submissions are valid

--- src/process/filter/index.ts
+++ src/process/filter/index.ts
@@ -1,78 +1,93 @@
-import { FilterContext, FilterScope, ProcessorFn, ProcessorFnSync, ProcessorInfo } from "types";
+import {
+  FilterContext,
+  FilterScope,
+  ProcessorFn,
+  ProcessorFnSync,
+  ProcessorInfo,
+} from 'types';
 import set from 'lodash/fp/set';
-import { Utils } from "utils";
-import { get } from "lodash";
-import { getComponentAbsolutePath } from "utils/formUtil";
-export const filterProcessSync: ProcessorFnSync<FilterScope> = (context: FilterContext) => {
-    const { scope, component } = context;
-    let { value } = context;
-    const absolutePath = getComponentAbsolutePath(component);
-    if (!scope.filter) scope.filter = {};
-    if (value !== undefined) {
-        const modelType = Utils.getModelType(component);
-        switch (modelType) {
-            case 'dataObject':
-                scope.filter[absolutePath] = {
-                    compModelType: modelType,
-                    include: true,
-                    value: { data: {} }
-                };
-                break;
-            case 'array':
-                scope.filter[absolutePath] = {
-                    compModelType: modelType,
-                    include: true,
-                };
-                break;
-            case 'object':
-                if (component.type !== 'container') {
-                    scope.filter[absolutePath] = {
-                        compModelType: modelType,
-                        include: true,
-                    };
-                }
-                break;
-            default:
-                scope.filter[absolutePath] = {
-                    compModelType: modelType,
-                    include: true,
-                };
-                break;
+import { Utils } from 'utils';
+import { get } from 'lodash';
+import { getComponentAbsolutePath } from 'utils/formUtil';
+export const filterProcessSync: ProcessorFnSync<FilterScope> = (
+  context: FilterContext
+) => {
+  const { scope, component } = context;
+  let { value } = context;
+  const absolutePath = getComponentAbsolutePath(component);
+
+  if (!scope.filter) scope.filter = {};
+  if (value !== undefined) {
+    const modelType = Utils.getModelType(component);
+    switch (modelType) {
+      case 'dataObject':
+        scope.filter[absolutePath] = {
+          compModelType: modelType,
+          include: true,
+          value: { data: {} },
+        };
+        break;
+      case 'array':
+        scope.filter[absolutePath] = {
+          compModelType: modelType,
+          include: true,
+        };
+        break;
+      case 'object':
+        if (component.type !== 'container') {
+          scope.filter[absolutePath] = {
+            compModelType: modelType,
+            include: true,
+          };
         }
+        break;
+      default:
+        scope.filter[absolutePath] = {
+          compModelType: modelType,
+          include: true,
+        };
+        break;
     }
+  }
 };
 
-export const filterProcess: ProcessorFn<FilterScope> = async (context: FilterContext) => {
-    return filterProcessSync(context);
+export const filterProcess: ProcessorFn<FilterScope> = async (
+  context: FilterContext
+) => {
+  return filterProcessSync(context);
 };
 
-export const filterPostProcess: ProcessorFnSync<FilterScope> = (context: FilterContext) => {
-    const { scope, component, submission } = context;
-    let filtered = {};
-    for (const path in scope.filter) {
-        const pathFilter = scope.filter[path];
-        if (pathFilter) {
-            let value = get(submission?.data, path) as any;
-
-            // when it's a dataModel Object, don't set values directly on the data object, let child fields do that.
-            // it can have extra data on updates, so pass all other values except data
-            // standard lodash set function will mutate original value, using the functional version so it doesn't
-            if (pathFilter.compModelType === 'dataObject') {
-                const { data, ...rest } = value;
-                filtered = set(path, rest)(filtered);
-            } else {
-                filtered = set(path, value)(filtered);
-            }
-        }
+export const filterPostProcess: ProcessorFnSync<FilterScope> = (
+  context: FilterContext
+) => {
+  const { scope, component, submission } = context;
+  let filtered = {};
+  for (const path in scope.filter) {
+    const pathFilter = scope.filter[path];
+    if (pathFilter.compModelType === 'array') {
+      continue;
     }
-    context.data = filtered;
+    if (pathFilter) {
+      let value = get(submission?.data, path) as any;
 
+      // when it's a dataModel Object, don't set values directly on the data object, let child fields do that.
+      // it can have extra data on updates, so pass all other values except data
+      // standard lodash set function will mutate original value, using the functional version so it doesn't
+      if (pathFilter.compModelType === 'dataObject') {
+        const { data, ...rest } = value;
+        filtered = set(path, rest)(filtered);
+      } else {
+        filtered = set(path, value)(filtered);
+      }
+    }
+  }
+  context.data = filtered;
 };
 
 export const filterProcessInfo: ProcessorInfo<FilterContext, void> = {
-    name: 'filter',
-    process: filterProcess,
-    processSync: filterProcessSync,
-    postProcess: filterPostProcess,
-    shouldProcess: (context: FilterContext) => true,
+  name: 'filter',
+  process: filterProcess,
+  processSync: filterProcessSync,
+  postProcess: filterPostProcess,
+  shouldProcess: (context: FilterContext) => true,
 };
