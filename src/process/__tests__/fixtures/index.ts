e6dd3cebc9fc7922b48b4456367132a9c5d909d9
mikekotikov <kmikecoder@gmail.com>
2024-07-27 22:56:34 +0200

    FIO-8730: Add test and refactor some already existing for similar cases

---
+++ src/process/__tests__/fixtures/index.ts
@@ -0,0 +1,7 @@
+import clearOnHideWithCustomCondition from './clearOnHideWithCustomCondition.json';
+import clearOnHideWithHiddenParent from './clearOnHideWithHiddenParent.json';
+import data1a from './data1a.json';
+import form1 from './form1.json';
+import subs from './subs.json';
+
+export { clearOnHideWithCustomCondition, clearOnHideWithHiddenParent, data1a, form1, subs };
