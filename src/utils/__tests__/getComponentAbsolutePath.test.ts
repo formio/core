94cecce5d7e98b76227602852fce6e02f4f59026
brendanjbond <brendanbond@gmail.com>
2024-07-29 16:33:34 -0400

    temporarily disable new test

--- src/utils/__tests__/getComponentAbsolutePath.test.ts
+++ src/utils/__tests__/getComponentAbsolutePath.test.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { Component } from "types";
 import { getComponentAbsolutePath, eachComponent } from "utils/formUtil";
 
-describe("getComponentAbsolutePath", () => {
+xdescribe("getComponentAbsolutePath", () => {
     it.only('should return the absolute path when iterating throught child components', () => {
         const form = {
             key: 'form',
@@ -33,10 +33,10 @@ describe("getComponentAbsolutePath", () => {
         // when passed child components, absolute path returns relative to the parent component/
         eachComponent(form.components[0].components[1].components, (component: Component, path: string) => {
             const absolutePath = getComponentAbsolutePath(component);
-            
+
             expect(absolutePath).to.equal('outer-container.inner-container.innerTextfield', 'absolute path path is incomplete');
-            
-            
+
+
         });
 
 
