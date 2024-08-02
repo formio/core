e6423084b5b701079e5950f70ba526a8c364e4f6
mikekotikov <kmikecoder@gmail.com>
2024-08-02 20:01:39 +0200

    FIO-8731: Add test cases

--- src/process/__tests__/fixtures/index.ts
+++ src/process/__tests__/fixtures/index.ts
@@ -1,7 +1,10 @@
 import clearOnHideWithCustomCondition from './clearOnHideWithCustomCondition.json';
 import clearOnHideWithHiddenParent from './clearOnHideWithHiddenParent.json';
+import skipValidForConditionallyHiddenComp from './skipValidForConditionallyHiddenComp.json';
+import skipValidForLogicallyHiddenComp from './skipValidForLogicallyHiddenComp.json';
+import skipValidWithHiddenParentComp from './skipValidWithHiddenParentComp.json';
 import data1a from './data1a.json';
 import form1 from './form1.json';
 import subs from './subs.json';
 
-export { clearOnHideWithCustomCondition, clearOnHideWithHiddenParent, data1a, form1, subs };
+export { clearOnHideWithCustomCondition, clearOnHideWithHiddenParent, skipValidForLogicallyHiddenComp,  skipValidForConditionallyHiddenComp, skipValidWithHiddenParentComp, data1a, form1, subs };
