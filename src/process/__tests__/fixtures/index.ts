a26e0845021880b1d10f9b26d0c3ec58cecd67dd
Mike Kotikov <54106407+mikekotikov@users.noreply.github.com>
2024-09-24 17:10:07 +0300

    FIO-9085: Fix components data removed from submission when conditional set for Address component value (#155)
    * FIO-9085: Fix submissions for conditional components related to address component
    
    * FIO-9085: Add small fixes

--- src/process/__tests__/fixtures/index.ts
+++ src/process/__tests__/fixtures/index.ts
@@ -3,9 +3,10 @@ import clearOnHideWithHiddenParent from './clearOnHideWithHiddenParent.json';
 import skipValidForConditionallyHiddenComp from './skipValidForConditionallyHiddenComp.json';
 import skipValidForLogicallyHiddenComp from './skipValidForLogicallyHiddenComp.json';
 import skipValidWithHiddenParentComp from './skipValidWithHiddenParentComp.json';
+import addressComponentWithOtherCondComponents from './addressComponentWithOtherCondComponents.json';
 import forDataGridRequired from './forDataGridRequired.json';
 import data1a from './data1a.json';
 import form1 from './form1.json';
 import subs from './subs.json';
 
-export { clearOnHideWithCustomCondition, clearOnHideWithHiddenParent, skipValidForLogicallyHiddenComp,  skipValidForConditionallyHiddenComp, skipValidWithHiddenParentComp, forDataGridRequired, data1a, form1, subs };
+export { addressComponentWithOtherCondComponents, clearOnHideWithCustomCondition, clearOnHideWithHiddenParent, skipValidForLogicallyHiddenComp,  skipValidForConditionallyHiddenComp, skipValidWithHiddenParentComp, forDataGridRequired, data1a, form1, subs };
