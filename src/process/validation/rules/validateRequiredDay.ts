01c7f2499dd91b26d66ed3d6245b182648fdb4af
Hanna Kurban <hannak@form.io>
2024-11-14 09:53:51 +0300

    FIO-9344 fixed require validation for day component

--- src/process/validation/rules/validateRequiredDay.ts
+++ src/process/validation/rules/validateRequiredDay.ts
@@ -39,11 +39,33 @@ export const validateRequiredDaySync: RuleFnSync = (context: ValidationContext) 
       'validate:validateRequiredDay',
     );
   }
-  const [DAY, MONTH, YEAR] = component.dayFirst ? [0, 1, 2] : [1, 0, 2];
-  const values = value.split('/').map((x) => parseInt(x, 10)),
-    day = values[DAY],
-    month = values[MONTH],
-    year = values[YEAR];
+  let [DAY, MONTH, YEAR] = component.dayFirst ? [0, 1, 2] : [1, 0, 2];
+  const values = value.split('/').map((x) => parseInt(x, 10));
+  let day = values[DAY];
+  let month = values[MONTH];
+  let year = values[YEAR];
+
+  if (values.length !== 3) {
+    if (component.fields.day.hide) {
+      MONTH = MONTH === 0 ? 0 : MONTH - 1;
+      YEAR = YEAR - 1;
+      day = 0;
+      month = values[MONTH];
+      year = values[YEAR];
+    }
+    if (component.fields.month.hide) {
+      DAY = DAY === 0 ? 0 : DAY - 1;
+      YEAR = YEAR - 1;
+      day = component.fields.day.hide && day === 0 ? 0 : values[DAY];
+      month = 0;
+      year = values[YEAR];
+    }
+    if (component.fields.year.hide) {
+      day = component.fields.day.hide && day === 0 ? 0 : values[DAY];
+      month = component.fields.month.hide && month === 0 ? 0 : values[MONTH];
+      year = 0;
+    }
+  }
 
   if (!day && component.fields?.day?.required) {
     return new FieldError('requiredDayField', context, 'day');
