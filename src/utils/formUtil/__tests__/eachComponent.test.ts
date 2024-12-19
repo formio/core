e00f32bfbafe03e4c126b1dce7ebed90cc411156
mikekotikov <kmikecoder@gmail.com>
2024-12-19 18:19:09 +0300

    FIO-9467: Fix rendering table component in wizard

--- src/utils/formUtil/__tests__/eachComponent.test.ts
+++ src/utils/formUtil/__tests__/eachComponent.test.ts
@@ -640,6 +640,75 @@ describe('eachComponent', function () {
     },
   ];
 
+  const components4 = [
+    {
+      title: "Page 1",
+      breadcrumbClickable: false,
+      buttonSettings: {
+        previous: true,
+        cancel: true,
+        next: true
+      },
+
+      key: "page1",
+      type: "panel",
+      label: "Page 1",
+      tableView: false,
+      components: [
+        {
+          label: "Text Field",
+          tableView: true,
+          key: "textField",
+          type: "textfield",
+          input: true
+        },
+      ],
+      input: false,
+      allowPrevious: false
+    },
+    {
+      title: "Page 2",
+      breadcrumbClickable: false,
+      buttonSettings: {
+        previous: true,
+        cancel: true,
+        next: true
+      },
+      key: "page2",
+      type: "panel",
+      label: "Page 2",
+      allowPrevious: false,
+      input: false,
+      tableView: false,
+      components: [
+        {
+          label: "Component",
+          cellAlignment: "left",
+          key: "table",
+          type: "components",
+          component: {
+            type: "table",
+            rows: [
+              [
+                {
+                  components: [
+                    {
+                      label: "Url",
+                      tableView: true,
+                      key: "url1",
+                      type: "url",
+                      input: true
+                    }
+                  ]
+                },
+              ]
+            ]
+          }
+        }
+      ]
+    },
+  ]
+
   it('should iterate through nested components in the right order', function () {
     let n = 1;
     eachComponent(components, (component: Component) => {
@@ -1081,5 +1150,19 @@ describe('eachComponent', function () {
         }
       },
     );
+  });
+
+  it('should not get error with table inside wizard', function () {
+    let isExistUrlComponentInTable;
+    eachComponent(
+      components4,
+      (component: Component) => {
+        if (component.key === 'url1') {
+          isExistUrlComponentInTable = true;
+        }
+      },
+      true,
+    );
+    expect(isExistUrlComponentInTable).to.be.equal(true);
   });
 });
