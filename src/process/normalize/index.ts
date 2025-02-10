bb680533f7cd07c718a62187f0354dbc6e1e6d0f
ICX\Tatsiana.Hashtold <tanya@form.io>
2025-02-10 11:35:35 +0300

    FIO-8118: removed datetime value from submission if null is submitted

--- src/process/normalize/index.ts
+++ src/process/normalize/index.ts
@@ -18,6 +18,7 @@ import {
   TimeComponent,
   NumberComponent,
   SurveyComponent,
+  DateTimeComponent,
 } from 'types';
 
 type NormalizeScope = DefaultValueScope & {
@@ -45,7 +46,10 @@ const isTextFieldComponent = (component: any): component is TextFieldComponent =
 const isTimeComponent = (component: any): component is TimeComponent => component.type === 'time';
 const isNumberComponent = (component: any): component is NumberComponent =>
   component.type === 'number';
-const isSurveyComponent = (component: any): component is SurveyComponent => component.type === 'survey';
+const isSurveyComponent = (component: any): component is SurveyComponent =>
+  component.type === 'survey';
+const isDateTimeComponent = (component: any): component is DateTimeComponent =>
+  component.type === 'datetime';
 
 const normalizeAddressComponentValue = (component: AddressComponent, value: any) => {
   if (!component.multiple && Boolean(component.enableManualMode) && value && !value.mode) {
@@ -411,6 +415,10 @@ export const normalizeProcessSync: ProcessorFnSync<NormalizeScope> = (context) =
     scope.normalize[path].normalized = true;
   } else if (isSurveyComponent(component)) {
     if (!data[path]) {
+      delete data[path];
+    }
+  } else if (isDateTimeComponent(component)) {
+    if (data[path] === null) {
       delete data[path];
     }
   }
